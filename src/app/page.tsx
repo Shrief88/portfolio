40b530919bbe081a11162c020b8622711321694a
Shrief Essam <shriefessam1999@gmail.com>
2025-03-17 23:40:15 +0200

    Add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,23 @@
 import Navbar from "@/components/Navbar";
-// import Hero from "@/sections/Hero";
-// import About from "@/sections/About";
-// import Experience from "@/sections/Experience";
-// import Work from "@/sections/Work";
-// import Contact from "@/sections/Contact";
-// import Footer from "@/components/Footer";
+import Hero from "@/sections/Hero";
+import About from "@/sections/About";
+import Experience from "@/sections/Experience";
+import Work from "@/sections/Work";
+import Contact from "@/sections/Contact";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div className="px-8 py-4">
+    <>
       <Navbar />
-      {/* <div className="flex flex-col gap-4">
+      <main className="px-8 mt-[var(--nav-height)]">
         <Hero />
         <About />
         <Experience />
         <Work />
         <Contact />
-      </div>
-      <Footer /> */}
-    </div>
+      </main>
+      <Footer />
+    </>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,56 @@
+"use client";
+
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
+
+const links = ["About", "Experience", "Work", "Contact"];
 
 const Navbar = () => {
-  const links = ["About", "Experience", "Work", "Contact"];
+  const [show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [shouldHasShadow, setShouldHasShadow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      // As long as the currentScrollY is greater than the lastScrollY, that means the user is scrolling down so we hide the navbar otherwise we show it
+      if (currentScrollY > lastScrollY) {
+        setShow(false);
+      } else {
+        setShow(true);
+        setShouldHasShadow(currentScrollY > 0);
+      }
+
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY]);
+
   return (
-    <div className="flex items-center justify-end gap-4 font-fira text-xs">
-      {links.map((link, index) => (
-        <Link key={link} href={`#${link.toLowerCase()}`}>
-          {`0${index + 1}.` + " " + link}
-        </Link>
-      ))}
-      <Button variant={"outline"} className="cursor-pointer">
-        Resume
-      </Button>
-    </div>
+    <nav
+      className={cn(
+        "px-8 fixed top-0 left-0 w-full text-xs font-fira bg-background transition-transform",
+        show ? "translate-y-0" : "-translate-y-full",
+        shouldHasShadow ? "drop-shadow-lg shadow-gray-900/50 backdrop-blur-md" : ""
+      )}
+    > 
+      <div className="h-[var(--nav-height)] flex items-center justify-end gap-8">
+        {links.map((link, index) => (
+          <Link key={link} href={`#${link.toLowerCase()}`}>
+            <p className="underline-animation">
+              {`0${index + 1}.` + " " + link}
+            </p>
+          </Link>
+        ))}
+        <Button variant={"outline"} className="cursor-pointer">
+          Resume
+        </Button>
+      </div>
+    </nav>
   );
 };
 
