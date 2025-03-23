55321ec23afc4e4c8f7eb3727724d9dbcf58725c
Shrief Essam <shriefessam1999@gmail.com>
2025-03-23 15:00:13 +0200

    Add mobile nav

---
+++ src/components/MobileNav.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { useState } from "react";
+import { Menu } from "lucide-react";
+import Link from "next/link";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button, buttonVariants } from "@/components/ui/button";
+
+interface Props {
+  items: string[];
+}
+
+const MobileNav = ({ items }: Props) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger asChild>
+        <Button size={"icon"} className={buttonVariants({ variant: "ghost" })}>
+          <Menu className="cursor-pointer" />
+        </Button>
+      </SheetTrigger>
+
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle className="hidden"></SheetTitle>
+          <SheetDescription className="hidden"></SheetDescription>
+        </SheetHeader>
+        <ul className="flex flex-col gap-4 py-6 justify-center items-center">
+          {items.map((item, index) => (
+            <li key={item}>
+              <Link href={`#${item.toLowerCase()}`}>
+                <p className="text-xs font-fira text-foreground">
+                  {`0${index + 1}.` + " " + item}
+                </p>
+              </Link>
+            </li>
+          ))}
+          <Button
+            variant={"outline"}
+            className="cursor-pointer text-xs font-fira text-foreground"
+            asChild
+          >
+            <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+              Resume
+            </a>
+          </Button>
+        </ul>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNav;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,7 @@ import { Button } from "./ui/button";
 import { cn } from "@/lib/utils";
 
 import useScroll from "@/hooks/useScroll";
+import MobileNav from "./MobileNav";
 
 const links = ["About", "Experience", "Work", "Contact"];
 
@@ -14,14 +15,17 @@ const Navbar = () => {
   return (
     <nav
       className={cn(
-        "px-8 fixed top-0 left-0 w-full bg-background transition-transform hidden md:block",
+        "px-2 md:px-8 fixed top-0 left-0 w-full bg-background transition-transform",
         show ? "translate-y-0" : "-translate-y-full",
         shouldHasShadow
           ? "drop-shadow-lg shadow-gray-900/50 backdrop-blur-md"
           : ""
       )}
     >
-      <div className="h-[var(--nav-height)] flex items-center justify-end gap-8">
+      <div className="md:hidden flex justify-end items-center py-3">
+        <MobileNav items={links} />
+      </div>
+      <div className="h-[var(--nav-height)] hidden md:flex items-center justify-end gap-8">
         {links.map((link, index) => (
           <Link key={link} href={`#${link.toLowerCase()}`}>
             <p className="underline-animation text-xs font-fira text-foreground">
